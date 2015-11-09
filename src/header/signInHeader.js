7e83d1306b3084836cce5af3e76bfabda042477d
limeng32 <limeng32@live.cn>
2015-11-09 18:47:02 +0800

    Develop signIn page.

---
+++ src/header/signInHeader.js
@@ -0,0 +1,6 @@
+var $ = require('node').all;
+module.exports = {
+    init:function(){
+        $('header').html('请登录');
+    }
+}
---
+++ src/signUpWrapper.js
@@ -0,0 +1,7 @@
+//初始化header模块
+var header = require('./header/header');
+header.init();
+
+//初始化article模块
+var signUp = require('./signUp/signUp');
+signUp.init();
